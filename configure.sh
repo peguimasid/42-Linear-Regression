3e69d3bc604d01f92dc88a44c58b0088ac3eb5cd
Guilhermo Masid <guilhermomasid@gmail.com>
2025-01-23 19:54:54 -0300

    add requirements and config file

---
+++ configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "Virtual env created!"
+    echo "Installing packages:"
+    pip install -r requirements.txt
+else
+    echo "Virtual env is not working!"
+fi

368559e6804bb586869bd227755e377c0ad58a88
Guilhermo Masid <guilhermomasid@gmail.com>
2025-01-26 12:32:15 -0300

    successfully run python via virtual env

--- configure.sh
+++ configure.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
 python3 -m venv venv
-source venv/bin/activate
+
+# source venv/bin/activate => bash
+
+source venv/bin/activate.fish
 if [[ "$VIRTUAL_ENV" != "" ]]; then
     echo "Virtual env created!"
     echo "Installing packages:"
---
+++ main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [3, 4, 2, 5, 6, 7, 8, 5, 6, 7]
+
+plt.scatter(x, y)
+
+plt.show()

95783a86fecad5e13c13d9ee65d35adf239b2147
Guilhermo Masid <guilhermomasid@gmail.com>
2025-01-29 18:36:27 -0300

    remove

--- train.py
+++ train.py
@@ -7,17 +7,8 @@ data = pd.read_csv('data.csv')
 x = data.iloc[:, 0]
 y = data.iloc[:, 1]
 
-# Perform linear regression using numpy.polyfit
-coefficients = np.polyfit(x, y, 1)
-slope, intercept = coefficients
-
-# Generate the regression line
-regression_line = slope * x + intercept
-
 plt.scatter(x, y, label='Data points')
-plt.plot(x, regression_line, color='red', label='Regression line')
 plt.xlabel('mileage')
 plt.ylabel('price')
 plt.legend()
-
 plt.show()

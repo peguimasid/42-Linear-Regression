6544de96d2f5c83d73d15f3a742f7712a1a1b6cc
Guilhermo Masid <guilhermomasid@gmail.com>
2025-01-30 18:36:22 -0300

    normalize data

--- train.py
+++ train.py
@@ -2,13 +2,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv("data.csv")
 
 x = data.iloc[:, 0]
 y = data.iloc[:, 1]
 
-plt.scatter(x, y, label='Data points')
-plt.xlabel('mileage')
-plt.ylabel('price')
+# Normalize the data
+x = (x - x.min()) / (x.max() - x.min())
+y = (y - y.min()) / (y.max() - y.min())
+
+plt.scatter(x, y, label="Data points")
+plt.xlabel("mileage")
+plt.ylabel("price")
 plt.legend()
+
 plt.show()

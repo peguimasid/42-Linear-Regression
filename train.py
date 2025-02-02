9cf31877e723c66b1cbba3627efdec7c0cb26a5e
Guilhermo Masid <guilhermomasid@gmail.com>
2025-02-02 10:12:41 -0300

    perfome gradient descent but not optimized

--- train.py
+++ train.py
@@ -8,11 +8,16 @@ x = data.iloc[:, 0]
 y = data.iloc[:, 1]
 
 # Normalize the data
+# x = (x - x.mean()) / x.std()
+# y = (y - y.mean()) / x.std()
 x = (x - x.min()) / (x.max() - x.min())
 y = (y - y.min()) / (y.max() - y.min())
 
 intercept = 0  # 1
 slope = 0  # -1.2
+
+learning_rate = 0.01
+iterations = 5000
 
 
 def compute_cost(x, y, intercept, slope):
@@ -22,6 +27,24 @@ def compute_cost(x, y, intercept, slope):
     return cost
 
 
+print(compute_cost(x, y, intercept, slope))
+
+
+def gradient_descent(x, y, intercept, slope):
+    m = len(y)
+    for _ in range(iterations):
+        predictions = intercept + slope * x
+        d_intercept = (1 / m) * sum(predictions - y)
+        d_slope = (1 / m) * sum((predictions - y) * x)
+
+        intercept = intercept - learning_rate * d_intercept
+        slope = slope - learning_rate * d_slope
+    return intercept, slope
+
+
+intercept, slope = gradient_descent(x, y, intercept, slope)
+
+print(intercept, slope)
 print(compute_cost(x, y, intercept, slope))
 
 line_x = np.linspace(x.min(), x.max(), 100)

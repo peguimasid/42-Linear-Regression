1c0c8ae8c6cfe399b0f22e087bb2e7592f9e0571
Guilhermo Masid <guilhermomasid@gmail.com>
2025-02-06 18:49:00 -0300

    start params

--- train.py
+++ train.py
@@ -11,3 +11,10 @@ y = data.iloc[:, 1]
 # Normalize the data
 x = (x - x.mean()) / x.std()
 y = (y - y.mean()) / y.std()
+
+# Start params
+intercept = 0
+slope = 0
+
+learning_rate = 0.01
+iterations = 1000

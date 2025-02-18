db493033c09410d70013633b82c6b5fdaf222346
Guilhermo Masid <guilhermomasid@gmail.com>
2025-02-18 19:50:35 -0300

    working

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ m = len(data)
 x = data.iloc[:, 0]
 y = data.iloc[:, 1]
 
-# Normalize the data
+# Z-score standardization
 x = (x - x.mean()) / x.std()
 y = (y - y.mean()) / y.std()
 
@@ -16,7 +16,7 @@ y = (y - y.mean()) / y.std()
 intercept = 20
 slope = 0
 learning_rate = 0.01
-iterations = 1000
+iterations = 5000
 
 
 def compute_cost(x, y, intercept, slope):
@@ -29,7 +29,7 @@ def compute_cost(x, y, intercept, slope):
 def gradient_descent(x, y, intercept, slope):
     cost_history = []  # Store cost at each iteration
 
-    for i in range(iterations):
+    for _ in range(iterations):
         # Compute predictions
         predictions = intercept + slope * x
 
@@ -40,11 +40,6 @@ def gradient_descent(x, y, intercept, slope):
         # Update parameters
         intercept -= learning_rate * d_intercept
         slope -= learning_rate * d_slope
-        print(f"{(learning_rate * sum(predictions - y) / m):.22f}")
-        print(f"{intercept}")
-
-        if i == 2:
-            break
 
         # Store the cost (optional, for monitoring)
         cost = compute_cost(x, y, intercept, slope)
@@ -72,6 +67,4 @@ def show_regression_line(x, y, intercept, slope):
 
 intercept, slope, cost_history = gradient_descent(x, y, intercept, slope)
 
-# print(intercept, slope)
-
-# show_regression_line(x, y, intercept, slope)
+show_regression_line(x, y, intercept, slope)

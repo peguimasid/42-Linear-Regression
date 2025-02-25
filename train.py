08e37f43fab233f2c9b9aa50335e58ce409560f8
Guilhermo Masid <guilhermomasid@gmail.com>
2025-02-25 20:10:59 -0300

    plot regression and cost history

--- train.py
+++ train.py
@@ -63,5 +63,5 @@ y = data.iloc[:, 1]
 slope = slope * y.std() / x.std()
 intercept = y.mean() - slope * x.mean()
 
-# plot_regression_line(x, y, intercept, slope)
+plot_regression_line(x, y, intercept, slope)
 plot_cost_history(cost_history)

29a53efd8dbb700fea141a005a9d4cb8f32801cb
Guilhermo Masid <guilhermomasid@gmail.com>
2025-02-26 18:55:47 -0300

    fix configure.sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,5 @@
 python3 -m venv venv
-
-# source venv/bin/activate => bash
-
-source venv/bin/activate.fish
+source venv/bin/activate
 if [[ "$VIRTUAL_ENV" != "" ]]; then
     echo "Virtual env created!"
     echo "Installing packages:"

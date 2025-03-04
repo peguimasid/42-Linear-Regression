f99730b3ecabbe1b4b4701b040e7ef0e7ef47668
Guilhermo Masid <guilhermomasid@gmail.com>
2025-03-04 17:58:53 -0300

    compute cost

--- scratch/train.py
+++ scratch/train.py
@@ -15,3 +15,15 @@ intercept = 0
 slope = 0
 learning_rate = 0.01
 iterations = 1000
+
+
+def compute_cost(x, y, intercept, slope):
+    predictions = [intercept + slope * val for val in x]
+    errors = [(predicted - actual) ** 2 for predicted, actual in zip(predictions, y)]
+    cost = (1 / (2 * m)) * sum(errors)
+    return cost
+
+
+print(compute_cost(x, y, 0, 0))  # 0.4791666666666668
+print(compute_cost(x, y, 1, 1))  # 2.278800278257564
+print(compute_cost(x, y, 3, -0.2))  # 4.834239944348486
